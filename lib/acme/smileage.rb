5494f464964baa9a64389a30bead099bed1c29b8
nicochan99 <nicochan990@yahoo.co.jp>
2014-04-09 16:45:54 +0900

    Acme::Smileage に birthday と homepage_link を追加

--- lib/acme/smileage.rb
+++ lib/acme/smileage.rb
@@ -1,10 +1,17 @@
+require "date"
+
 require "acme/smileage/members"
 require "acme/smileage/discography"
 require "acme/smileage/version"
 
 module Acme
   class Smileage
+    attr_reader :birthday, :homepage_link
+
     def initialize
+      @birthday = Date.new(2009, 4, 4)
+      @homepage_link = "http://s-mileage.jp"
+
       @members = Acme::Smileage::Members.new
       @discography = Acme::Smileage::Discography.new
     end
